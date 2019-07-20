7fe33a58ff95f922590d1eda4ad48de0e6205fc4
Andrew Young <andrew@youngshome.com>
2019-07-19 20:58:11 -0700

    Edit - Staff Changes

--- website-staff/src/components/General/Header.vue
+++ website-staff/src/components/General/Header.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="headercomp flex flex-space-between">
     <div class="flex">
-      <div class="flex flex-vert-cent flex-space-around link right-line" id="logo">
+      <div class="flex flex-vert-cent flex-space-around link " id="logo">
         <div id="logo-icon" class="icon"></div>
       </div>
-      <div class="flex flex-vert-cent flex-space-around link right-line">
+      <div class="flex flex-vert-cent flex-space-around link ">
         <p>Home</p>
+      </div>
+      <div class="flex flex-vert-cent flex-space-around link ">
+        <p>Camp</p>
+      </div>
+      <div class="flex flex-vert-cent flex-space-around link ">
+        <p>Staff</p>
+      </div>
+      <div class="flex flex-vert-cent flex-space-around link ">
+        <p>Info</p>
       </div>
     </div>
     <div class="flex">
-      <div class="flex flex-vert-cent flex-space-around link left-line" id="notification">
+      <div class="flex flex-vert-cent flex-space-around link " id="notification">
         <div id="inbox-icon" class="icon left"></div>
       </div>
-      <div class="flex flex-vert-cent flex-space-around link left-line" id="profile">
+      <div class="flex flex-vert-cent flex-space-around link" id="profile">
         <div id="profile-icon" class="icon left"></div>
         <p>{{user.username}}</p>
       </div>
@@ -41,12 +50,14 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Slab:700&display=swap');
 /* General */
 .headercomp {
   width: 100vw;
   height: 65px;
   background-color: rgb(44, 129, 220);
   box-shadow: 0px 3px 3px rgb(190, 190, 190);
+  border-bottom: 5px solid white;
 }
 </style>
 
@@ -55,35 +66,22 @@ export default {
 .link {
   height: 100%;
   min-width: 50px;
-  border: 1px solid rgb(199, 199, 199);
-  padding: 0px 15px;
+  padding: 0px 30px;
   transition: all .2s ease;
   cursor: pointer;
-  color: #8b98a7;
+  color: #fefefe;
+  font-size: 1.3em;
+  font-weight: bolder; 
+  font-family: 'Roboto Slab', serif;
 }
 
 .active {
   background-color: rgba(224, 244, 253, 0.5) !important;
-  color: #2c3e50;
+  color: #fefefe;
 }
 
-.link:hover:not(.active) {
-  padding: 0px 20px;
-  background-color: rgb(49, 163, 216);
-  color: white;
-}
 
-.left-line {
-  border-right: 0px;
-  border-top: 0px;
-  border-bottom: 0px;
-}
 
-.right-line {
-  border-left: 0px;
-  border-top: 0px;
-  border-bottom: 0px;
-}
 
 p {
   font-weight: lighter;
@@ -116,8 +114,8 @@ p {
 }
 
 #logo-icon {
-  width: 40px;
-  height: 40px;
+  width: 35px;
+  height: 35px;
   background-image: url("../../assets/General/logo.png");
   opacity: .8;
   transition: all .2s ease;
@@ -129,6 +127,12 @@ p {
 
 #inbox-icon {
   background-image: url("../../assets/General/inbox.png");
+  opacity: .8;
+  transition: all .2s ease;
+}
+
+#profile-icon {
+  background-image: url("../../assets/General/profile.png");
   opacity: .8;
   transition: all .2s ease;
 }
