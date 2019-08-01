8f69ef894d2932ee5a56aae0a05e5ee8a06ad184
Uriyah Ann <uriyahruiz1@gmail.com>
2019-07-31 20:06:15 -0700

    Add - curriculum calls

--- server/curriculumApp.py
+++ server/curriculumApp.py
@@ -1,79 +1,126 @@
 from flask import Flask, render_template, redirect, request, url_for, jsonify
 from pymongo import MongoClient
+
 from classes import CreateCourse
 import hashlib, binascii, os
 from pprint import pprint
+#import datetime
 
 
 #config
 app = Flask(__name__)
 client = MongoClient()
-db = client.staff
+db = client.db
+lessons = db.lessons
 
-def createCourse(form):
-    course_name = form.course_name.data
-    course_compiler =  form.course_compiler.data
-    course_color = form.course_compiler.data
 
-    ch_number = form.ch_number.data
-    ch_name = form.ch_name.data
-    ch_color = form.ch_color.data
-    db.curriculum.insert_one({
-        #init: course, single chapter, single lesson (to be appended)
-        "course_name" : course_name,
-        "course_compiler" : course_compiler,
+def createLesson(form):
+    course = form.course.data
+    course_color = form.course_color.data
+    course_progress =  form.course_progress.data
+    course_exp_date =  form.course_exp_data
+    chapter_number =  form.chapter_number.data
+    chapter_title = form.chapter_title.data
+    section_number  = form.section_number.data
+    section_title = form.section_title.data
+
+    db.lessons.insert_one({
+        "course" : course,
         "course_color" : course_color,
-        "course_exp_data" : 0,
-        "chapter_list" : [
-            {
-               #single chapter sub-document
-               "ch_number" : ch_number,
-               "ch_name" : ch_name,
-               "ch_color" : ch_color,               
-               "ch_lessons" : [
-                   {
-                       #single lesson sub-document
-                       "lesson_number" : "",
-                       "lesson_name" : "",
-                       "lesson_acts" : [
-                           {
-                               #single lesson activity sub-document
-                               "lesson_act_id" : 0,
-                               "type" : "", #exercise, quiz,example
-                               "name" : "",
-                               "title" : "",
-                               "content" : []
-                        }]
-                }] 
-            }
-    ]})
-    return 
-
-def viewAllCourses():
-    print("Printing all documents in collection CURRICULUM...")
-    cursor = db.curriculum.find()
-    for document in cursor:
-        pprint(document)
+        "course_progress" : course_progress,
+        "course_exp_date" : course_exp_date,
+        "chapter_number" : chapter_number,
+        "chapter_title" : chapter_title,
+        "section_number" : section_number,
+        "section_title" : section_title,
+        "data"  : []  #lesson content
+    })
+    
+def main():
+    db.lessons.delete_many({})
+    db.lessons.insert_many(
+        {
+            "course" : "Python",
+            "chapter_number" : 1,
+            "chapter_title" : "Variables",
+            "section" : 1,
+            "section_title" : "Random Section Title1",
+            "data"  : []  #lesson content
+        },
+        {
+            "course" : "Python",
+            "chapter_number" : 1,
+            "chapter_title" : "Variables",
+            "section" : 2,
+            "section_title" : "Random Section Title2",
+            "data"  : []  #lesson content
+        },
+        {
+            "course" : "Python",
+            "chapter_number" : 1,
+            "chapter_title" : "Variables",
+            "section" : 2,
+            "section_title" : "Random Section Title2",
+            "data"  : []  #lesson content
+        },
+        {
+            "course" : "Java",
+            "chapter_number" : 1,
+            "chapter_title" : "Introduction to Java",
+            "section" : 2,
+            "section_title" : "Random Section Title1",
+            "data"  : []  #lesson content
+        },
+        {
+            "course" : "Java",
+            "chapter_number" : 1,
+            "chapter_title" : "Introduction to Java",
+            "section" : 2,
+            "section_title" : "Random Section Title2",
+            "data"  : []  #lesson content
+        },
+        {
+            "course" : "Java",
+            "chapter_number" : 1,
+            "chapter_title" : "Introduction to Java",
+            "section" : 2,
+            "section_title" : "Random Section Title3",
+            "data"  : []  #lesson content
+        }
+    )
     return
 
-@app.route("/displayCourses", methods = ['POST'])
-#display all courses & data
-def displayCourses() :
-    output = []
-    for s in db.curriculum.find() :
-        output.append(s)
+main()
+
+@app.route("/student/courses", methods = ['GET'])
+def courses():
+    cursor = db.lessons.find()
+    if cursor : 
+        output = []
+        for document in cursor:
+            if cursor :
+                course = {
+                    "course_name" : document["course"],
+                    "course_color" : document["course_color"],
+                    "course_progress" : document["course_progress"],
+                    "course_exp_date" : document["course_exp_date"]
+                }
+                output.append(course)
+    else : 
+        output = "no data in lessons collection"
+
     return jsonify({"result" : output})
 
-@app.route("/courseCreation", methods = ['GET','POST'])
-def courseCreation():
-    #create form
-    cform = CreateCourse(prefix = 'cform') #create Course
-
-    #response
-    if cform.validate_on_submit and cform.create.data:
-        return createCourse(cform)
-    
-    return render_template("",cform = cform)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/student/courses/<course>", methods = ['GET'])
+def oneCourse():
+    cursor = db.lessons.find({"course" : "Python"})
+    if cursor :
+        for document in cursor:
+            output = {
+                "course_name" : document["course"],
+                "course_color" : document["course_color"],
+                "course_progress" : document["course_progress"],
+                "course_exp_date" : document["course_exp_date"]
+            }
+    output  = "No course Python in lessons collection"
+    return jsonify({"result" : output})
